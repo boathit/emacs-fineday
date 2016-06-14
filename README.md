81018e712ed1d0d33130bd57d5b0d59692d584e3
Fineday <xiucheng90@gmail.com>
2016-06-14 11:48:30 +0800

    auto completion

--- README.md
+++ README.md
@@ -20,9 +20,11 @@ Monokai
 
 * Basic setting up, e.g., show line number, set font and appearance
 * Save all backup files in the specified path `~/.emacs.d/emacs-backup`
+* Set TAB as completion (default M-TAB)
 * Electrical auto indent
 * Highlight matching parentheses
 * Copy exectutable path from shell (copy environment variables from `~/.zshrc`)
+* Auto completion (trigger ac only when pressing TAB)
 * Colorize delimiters (essential for elegantly writing Clojure or any other Lisp-like language)
-* Julia mode and running julia shell
-* Clojure mode
+* Julia mode (enable latex substitute with TAB)
+* Clojure mode, Racket mode
--- init.el
+++ init.el
@@ -1,8 +1,11 @@
 ;; This is the package sources we would like to use
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+;			 ("melpa" . "http://melpa.milkbox.net/packages/")
+			 ("melpa" . "http://melpa.org/packages/")))
 
+(require 'package)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 ;; Show line number
 (global-linum-mode)
 
@@ -23,16 +26,19 @@
 (setq auto-save-file-name-transforms
       `((".*" ,"~/.emacs.d/emacs-backup" t)))
 
+
 ;; Enable auto indent
 (electric-indent-mode 1)
 
 ;; Highlight matching parentheses
 (show-paren-mode 1)
 
+;; Set tab as global completion
+(setq tab-always-indent 'complete)
+
 
 ;; ------------------------------------------------------------------------
 ;; Commonly used packages
-
 (package-initialize)
 
 ;; Copy executable path from shell [package-install exec-path-from shell]
@@ -41,7 +47,6 @@
 ;; Comment these two lines if you don't have Gurobi and Mosek license
 (exec-path-from-shell-copy-env "GRB_LICENSE_FILE")
 (exec-path-from-shell-copy-env "MOSEKLM_LICENSE_FILE")
-
 
 ;; Colorize delimiters [package-install rainbow-delimiters]
 (require 'rainbow-delimiters)
@@ -54,29 +59,38 @@
   (let ((inhibit-read-only t))
     (erase-buffer)
     (eshell-send-input)))
+
 (add-hook 'eshell-mode-hook
 	  '(lambda()
 	     (local-set-key (kbd "C-l") 'eshell-clear-buffer)))
+
+;; Auto-Complete
+;(require 'auto-complete-config)
+;(add-to-list 'ac-dictionary-directories "~/.emacs.d/elpa/auto-complete-20160416.604/dict")
+(ac-config-default)
+;; Trigger ac only when pressing TAB
+(setq ac-auto-start nil)
+(ac-set-trigger-key "TAB")
 
 ;; --------------------------------------------------------------------------
 ;; Language mode
 
 ;; Julia mode, available in github
-(add-to-list 'load-path "~/.emacs.d/elpa/")
-(require 'julia-mode)
+;(add-to-list 'load-path "~/.emacs.d/elpa/")
+;(require 'julia-mode)
 
-;; Julia shell, available in github
-(add-to-list 'load-path "~/.emacs.d/elpa/julia-shell-mode/")
-(require 'julia-shell)
-;; Interact with julia-mode
-(defun my-julia-mode-hooks ()
-  (require 'julia-shell-mode))
-(add-hook 'julia-mode-hook 'my-julia-mode-hooks)
-(define-key julia-mode-map (kbd "C-c C-c") 'julia-shell-run-region-or-line)
-(define-key julia-mode-map (kbd "C-c C-s") 'julia-shell-save-and-go)
+;; Using ess-julia-mode to support completion
+(load "~/Github/ESS/lisp/ess-site")
+(setq inferior-julia-program-name "julia")
+
+(define-key ess-julia-mode-map (kbd "TAB") 'julia-latexsub-or-indent)
+(global-set-key (kbd "<C-tab>") 'julia-latexsub-or-indent)
 
 ;; ---------------------------------------------------------------------------
 
 ;; Clojure mode [package-install clojure-mode]
 (require 'clojure-mode)
 
+;; Org setting
+(setq org-confirm-babel-evaluate nil)
+(add-hook 'org-babel-after-execute-hook 'org-display-inline-images 'append)
